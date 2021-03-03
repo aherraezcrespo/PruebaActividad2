dd534421d648d20447612cdb67c6f39c90a5f324
Alejandro <78168951+aherraezcrespo@users.noreply.github.com>
2021-03-03 17:47:16 +0100

    UnitTest Cociente

--- src/ed/ted/javadoc/TestCociente.java
+++ src/ed/ted/javadoc/TestCociente.java
@@ -19,32 +19,72 @@ class TestCociente {
 	public void tearDown() {
 	}
 	
+	// HAPPY PATHS
 
 	@Test
-	void testCocienteReales() {
-		fail("Not yet implemented");
+	void elCocienteDe5Entre2DebeSer2Coma5() {
+		// Arrange - Preparar
+		double dividendo = 5;
+		double divisor = 2;
+		double esperado = 2.5;
+		// Act - Ejecutar 
+		double resultado = cociente.cocienteReales(dividendo, divisor);
+		// Assert - Comprobar 
+		assertEquals(esperado, resultado,"la division de 5 entre 2 es 2.5");
 	}
 
 	@Test
-	void testCocienteEnteros() {
-		fail("Not yet implemented");
+	void elCocienteDe10Entre5DebeSer2() {
+		// Arrange - Preparar
+		int dividendo = 10;
+		int divisor = 5;
+		int esperado = 2;
+		// Act - Ejecutar
+		int resultado = cociente.cocienteEnteros(dividendo, divisor);
+		// Assert - Comprobar 
+		assertEquals(esperado, resultado,"la division de 10 entre 5 es 2");
 	}
 
 	@Test
-	void testCocienteInverso() {
-		fail("Not yet implemented");
+	void elInversoDe2DebeSer0Coma5() {
+		// Arrange - Preparar 
+		int base = 2;
+		double esperado = 0.5;
+		// Act - Ejecutar 
+		double resultado = cociente.cocienteInverso(base);
+		// Asser - Comprobar
+		assertEquals(esperado, resultado,"el inverso de 2 es 0.5");
+		
 	}
 
 	@Test
-	void testCocienteRaiz() {
+	void laRaizCuadradaDe25DebeSer5() {
 		// Arrange - Preparar
 		int base = 25;
 		int esperado = 5;
-		// 2 Act - Ejecutar
+		// Act - Ejecutar
 		double resultado = cociente.cocienteRaiz(base);
 		// Assert - Comprobar 
 		assertEquals(esperado, resultado,"la raiz de 25 es 5");
 	}
+	
+	// CASOS ESPECIALES 
+	
+	@Test
+	void laRaizCuadradaDeUnNumeroNegativoEsNaN() {
+		// Arrange - Preparar
+		int base = -9;
+		double esperado;
+		esperado = Double.NaN;
+		// Act - Ejecutar
+		double resultado = cociente.cocienteRaiz(base);
+		// Assert - Comprobar 
+		assertEquals(esperado, resultado,"la raiz de -9 debe ser 3");
+	}
+	
+	
+	
+	
 
 }
 
